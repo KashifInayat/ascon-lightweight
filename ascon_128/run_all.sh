c1ceb959da75dde5db8e3f18a241179918d8c45e
kinayat <kashif.inayat@bsc.es>
2024-05-28 12:37:39 +0200

    :pin: script modification

--- ascon_128/run_all.sh
+++ ascon_128/run_all.sh
@@ -6,36 +6,36 @@
 
 
 ##Removing
-node="32nm"
-dir=("encrypt_1block_128"  "encrypt_2blocks_128"  "encrypt_3blocks_128"  "encrypt_6blocks_128"	"ascon_encrypt") 
-for str in ${dir[@]}; do
- 	cd ./$str
-	rm -r input*
-	rm -r pdk*
-	rm -r scripts*
-	rm -r *.py
-	rm -r Make*
- 	cd ../	
-done
-
-# ## Running all
 # node="32nm"
 # dir=("encrypt_1block_128"  "encrypt_2blocks_128"  "encrypt_3blocks_128"  "encrypt_6blocks_128"	"ascon_encrypt") 
 # for str in ${dir[@]}; do
-# 	cd ./$str
-# 	mkdir work_dc
-# 	python3 run_synthesis.py --pdk $node
-# 	python3 run_power.py --pdk $node
-# 	python3 collect_all.py  
-# 	mkdir pdk$node
-# 	mv work_dc ./pdk$node
-# 	mv flis* ./pdk$node
-# 	mv nflis* ./pdk$node
-# 	mv synth*  ./pdk$node
-# 	mv log_*  ./pdk$node
-# 	rm -r work_*
-# 	cd ../	
+#  	cd ./$str
+# 	rm -r input*
+# 	rm -r pdk*
+# 	rm -r scripts*
+# 	rm -r *.py
+# 	rm -r Make*
+#  	cd ../	
 # done
+
+# ## Running all
+node="32nm"
+dir=("encrypt_1block_128"  "encrypt_2blocks_128"  "encrypt_3blocks_128"  "encrypt_6blocks_128"	"ascon_encrypt") 
+for str in ${dir[@]}; do
+	cd ./$str
+	mkdir work_dc
+	python3 run_synthesis.py --pdk $node
+	python3 run_power.py --pdk $node
+	python3 collect_all.py  
+	mkdir pdk$node
+	mv work_dc ./pdk$node
+	mv flis* ./pdk$node
+	mv nflis* ./pdk$node
+	mv synth*  ./pdk$node
+	mv log_*  ./pdk$node
+	rm -r work_*
+	cd ../	
+done
 
 ##copying
 
